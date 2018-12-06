27bab30425955bbdbf64d2cece6919f58d73f1eb
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-12-06 12:06:30 +0100

    s;lakjdf

--- CP2/Connection.cs
+++ CP2/Connection.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
 
 class Connection
 {
-    public void Connect()
-    {
+    public StreamReader Read;
+    public StreamWriter Write;
 
+    public Connection(int port)
+    {
+        TcpClient client = new TcpClient("localhost", port);
+        Read = new StreamReader(client.GetStream());
+        Write = new StreamWriter(client.GetStream());
     }
 }
 
