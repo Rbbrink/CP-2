33486ba86133fc8a39d7a95d7197d50fcb0777e0
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-12-06 12:20:48 +0100

    skjffjfdjfd

--- CP2/Connection.cs
+++ CP2/Connection.cs
@@ -2,22 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Net.Sockets;
+using System.Net;
+using System.Threading;
 
 class Connection
 {
     public StreamReader Read;
     public StreamWriter Write;
 
+    //Dit is de client
     public Connection(int port)
     {
         TcpClient client = new TcpClient("localhost", port);
         Read = new StreamReader(client.GetStream());
         Write = new StreamWriter(client.GetStream());
         Write.AutoFlush = true;
-        //;asfsajfs
+
+        Write.WriteLine("Poort: " + Program.thisport);
+
+        new Thread(ReaderThread).Start();
+    }
+
+    //Dit is de server
+    public Connection(StreamReader read, StreamWriter write)
+    {
+        Read = read; Write = write;
+
+        // Start het reader-loopje
+        new Thread(ReaderThread).Start();
+    }
+
+    public void ReaderThread()
+    {
+        try
+        {
+            while (true)
+                Console.WriteLine(Read.ReadLine());
+        }
+        catch { } // Verbinding is kennelijk verbroken
     }
 }
 
--- CP2/Program.cs
+++ CP2/Program.cs
@@ -7,15 +7,18 @@ using System.Threading.Tasks;
 
 class Program
 {
+    static public int thisport;
+
     static void Main(string[] args)
     {
         Program p = new Program();
         Console.Title = "poortnummer" + args[0];
-        p.Initialize();
+        p.Initialize(args);
     }
 
-    public void Initialize()
+    public void Initialize(String[] args)
     {
+        thisport = int.Parse(args[0]);
         Console.ReadLine();
     }
 }
