c76ff4c12f39e7000e4a5b20da43462f3d5d138f
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-12-06 11:23:00 +0100

    deze zooi

--- CP2/Program.cs
+++ CP2/Program.cs
@@ -7,24 +7,16 @@ using System.Threading.Tasks;
 
 class Program
 {
-    int ownNumber;
-
     static void Main(string[] args)
     {
         Program p = new Program();
-
+        Console.Title = "poortnummer" + args[0];
         p.Initialize();
     }
 
     public void Initialize()
     {
-        string[] input = Console.ReadLine().Split(' ');
-        ownNumber = int.Parse(input[0]);
-
-        for (int i = 0; i < input.Length; i++)
-        {
-            //read nodenumbers
-        }
+        Console.ReadLine();
     }
 }
 
